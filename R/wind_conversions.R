6dc08c030b0147ac4ab897df189ca9faa93381b6
Brooke Anderson <brooke.anderson@colostate.edu>
2016-09-19 14:34:04 -0600

    Change `remove_forward_speed` to use Phadke equation

--- R/wind_conversions.R
+++ R/wind_conversions.R
@@ -20,6 +20,7 @@
 #' @export
 calc_gradient_speed <- function(sustained_vmax){
   Vmax <- sustained_vmax / 0.9
+  return(Vmax)
 }
 
 #' Remove forward speed estimate from maximum wind speed
@@ -30,12 +31,26 @@ calc_gradient_speed <- function(sustained_vmax){
 #' @inheritParams will1a
 #' @inheritParams latlon_to_meters
 #' @inheritParams calc_forward_speed
+#' @inheritParams calc_gradient_speed
 #'
 #' @return A numerical vector with the maximum storm wind speed, with forward
 #' storm motion speed removed, in m / s.
-remove_forward_speed <- function(Vmax, phi_1, L_1, time_1, phi_2, L_2, time_2){
-  forward_speed <- calc_forward_speed(phi_1, L_1, time_1, phi_2, L_2, time_2)
-  Vmax <- Vmax - forward_speed
-  Vmax[Vmax < 0] <- 0
-  return(Vmax)
+#'
+#' @details This function is based on equation 12 (and accompanying
+#'    text) in Phadke et al. 2003. Based on this paper, the correction
+#'    factor for forward speed is at its highest at the radius of
+#'    maximum winds, where it equals 0.5 times the forward wind speed.
+#'
+#' @references
+#'
+#' Padke AC, Martino CD, Cheung KF, and Houston SH. 2003. Modeling of
+#'    tropical cyclone winds and waves for emergency management. Ocean
+#'    Engineering 30:553-578.
+remove_forward_speed <- function(sustained_vmax, phi_1, L_1, time_1,
+                                 phi_2, L_2, time_2){
+  forward_speed <- calc_forward_speed(phi_1, L_1, time_1,
+                                      phi_2, L_2, time_2)
+  sustained_vmax <- sustained_vmax - 0.5 * forward_speed
+  sustained_vmax[sustained_vmax < 0] <- 0
+  return(sustained_vmax)
 }
