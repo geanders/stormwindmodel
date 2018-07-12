8d30ab68382d9885068c08546a00dbc15aaacd65
Elizabeth Tennant <elizabethtennant@dhcp-ccc-1019.eduroam.cornell.edu>
2018-07-12 12:02:45 -0400

    global landmask data (-60 to 60 latitude; 180 to 540 longitude)

--- data-raw/add_landseamask.R
+++ data-raw/add_landseamask.R
@@ -1,5 +1,6 @@
 landmask <- readr::read_csv("data-raw/landseamask.csv",
-                            col_names = c("longitude", "latitude", "land")) %>%
+                            col_names = c("longitude", "latitude", "land"),
+                            col_types = "ddi") %>%
   dplyr::mutate(land = factor(land, levels = c(1, 0), labels = c("land", "water")))
 
 # ggplot(landmask, aes(x = longitude, y = latitude, color = land)) +
