a209340d01d97c046c291b5018696064e6cb231b
Brooke Anderson <brooke.anderson@colostate.edu>
2021-04-28 14:56:19 -0600

    Add more cases to test for checking land / sea

--- tests/testthat/test_landmask_comparisons.R
+++ tests/testthat/test_landmask_comparisons.R
@@ -10,7 +10,13 @@ ex_storms <- tribble(
   "Laura", "North Atlantic", 20.22, -76.86, TRUE,
   "Laura", "North Atlantic", 19.40, -74.40, FALSE,
   "Laura", "North Atlantic", 18.57, -70.17, TRUE,
-  "Laura", "North Atlantic", 17.90, -67.50, FALSE
+  "Laura", "North Atlantic", 17.90, -67.50, FALSE,
+  "Florence", "North Atlantic", 34.31, -77.20, FALSE,
+  "Florence", "North Atlantic", 34.20, -77.90, TRUE,
+  "Michael", "North Atlantic", 29.37, -85.93, FALSE,
+  "Michael", "North Atlantic", 30.20, -85.40, TRUE,
+  "Michael", "North Atlantic", 36.50, -77.70, TRUE,
+  "Michael", "North Atlantic", 39.10, -70.60, FALSE
 )
 
 test_that("Land mask comparison correct for North Atlantic basin storm", {
