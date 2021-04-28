ac75fffc9776c10a63d344203e5ed921271ed286
Brooke Anderson <brooke.anderson@colostate.edu>
2021-04-28 14:02:05 -0600

    Use newest Atlantic landsea mask from Andrea
    This includes Puerto Rico, some other Gulf areas that were not included in the original landsea mask.

--- data-raw/add_landseamask.R
+++ data-raw/add_landseamask.R
@@ -1,4 +1,4 @@
-landmask <- readr::read_csv("data-raw/landseamask.csv",
+landmask <- readr::read_csv("data-raw/landmask_0p2deg_15to50N_260to300E.csv",
                             col_names = c("longitude", "latitude", "land")) %>%
   dplyr::mutate(land = factor(land, levels = c(1, 0), labels = c("land", "water")))
 
