1b31232a8ea9eb7bab119a6623e74533f3151a66
Brooke Anderson <brooke.anderson@colostate.edu>
2022-04-12 09:47:35 -0600

    Add data on one other Western Pacific storm

--- data-raw/create_saomai_tracks.R
+++ data-raw/create_saomai_tracks.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(usethis)
 
 saomai_tracks <- read_csv("data-raw/ibtracs.since1980.list.v04r00.csv") %>%
   filter(NAME == "SAOMAI" & SEASON == 2006) %>%
@@ -6,3 +7,10 @@ saomai_tracks <- read_csv("data-raw/ibtracs.since1980.list.v04r00.csv") %>%
   select(iso_time, lat, lon, usa_wind, usa_rmw)
 
 use_data(saomai_tracks, overwrite = TRUE)
+
+mangkhut_tracks <- read_csv("data-raw/ibtracs.since1980.list.v04r00.csv") %>%
+  filter(NAME == "MANGKHUT" & SEASON == 2018) %>%
+  rename_all(str_to_lower) %>%
+  select(iso_time, lat, lon, usa_wind, usa_rmw)
+
+use_data(mangkhut_tracks, overwrite = TRUE)
